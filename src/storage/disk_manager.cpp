5ca64077d92c519bd2e0daf6368119f5a18650c2
wmz <1815960674@qq.com>
2026-05-10 00:06:49 +0800

    完成write_page函数

--- src/storage/disk_manager.cpp
+++ src/storage/disk_manager.cpp
@@ -29,9 +29,17 @@ DiskManager::DiskManager() { memset(fd2pageno_, 0, MAX_FD * (sizeof(std::atomic<
 void DiskManager::write_page(int fd, page_id_t page_no, const char *offset, int num_bytes) {
     // Todo:
     // 1.lseek()定位到文件头，通过(fd,page_no)可以定位指定页面及其在磁盘文件中的偏移量
+    off_t offset_in_file = lseek(fd,page_no * PAGE_SIZE, SEEK_SET);
+    if(offset_in_file == -1) {
+        // 如果lseek()函数调用失败，抛出InternalError异常
+        throw InternalError("DiskManager::write_page Error: lseek failed");
+    }
     // 2.调用write()函数
-    // 注意write返回值与num_bytes不等时 throw InternalError("DiskManager::write_page Error");
-
+    ssize_t bytes_written = write(fd, offset, num_bytes);
+    // 如果write()函数返回的字节数与num_bytes不等，抛出InternalError异常
+    if(bytes_written != num_bytes) {
+        throw InternalError("DiskManager::write_page Error: write failed");
+    }
 }
 
 /**
